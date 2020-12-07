40c2d0e422d063c70b356f9420ab57a1442fbfdc
ngost <hm5207@gmail.com>
2020-12-07 18:08:48 +0900

    audio

--- Root/QtStart.py
+++ Root/QtStart.py
@@ -1,12 +1,11 @@
 # 모듈 임포트 sys, PyQt5의 QApplication과 QWidget 모듈, wxPython
 import sys, os
 import wx
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtMultimedia
 # from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QAction, qApp, QMainWindow
 from PyQt5.QtGui import *
-from PyQt5.QtCore import Qt,QSize
-
-
+from PyQt5.QtCore import Qt,QSize,QDir,QUrl,QCoreApplication
+from PyQt5.QtMultimedia import QAudio, QAudioDeviceInfo, QAudioFormat, QAudioOutput,QMediaContent
 # App 관련 Class 생성
 class AppForm(QtWidgets.QWidget):
     # 기본 스크린 사이즈
@@ -34,6 +33,30 @@ class AppForm(QtWidgets.QWidget):
     #
     #     # self.setGeometry(300, 300, 300, 200)
 
+    def playBackgroundSound(self):
+
+        CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+        filename = os.path.join(CURRENT_DIR, "bgm_sample.wav")
+        QtMultimedia.QSound.play(filename)
+
+        # filename = os.path.join(CURRENT_DIR, "bgm.mp3")
+        # player = QtMultimedia.QMediaPlayer()
+        # print(filename)
+        #
+        # def handle_state_changed(state):
+        #
+        #     if state == QtMultimedia.QMediaPlayer.PlayingState:
+        #         print("started")
+        #     elif state == QtMultimedia.QMediaPlayer.StoppedState:
+        #         print("finished")
+        #         QCoreApplication.quit()
+        #
+        # player.stateChanged.connect(handle_state_changed)
+        # url = QUrl.fromLocalFile(filename)
+        # player.setMedia(QtMultimedia.QMediaContent(url))
+        #
+        # player.play()
+
     def init_ui(self):
         #set title
         self.setWindowTitle("Ai Application Demo with pyQt5")
@@ -51,11 +74,14 @@ class AppForm(QtWidgets.QWidget):
         # set background img
         #self.setStyleSheet('background-image: url(exit.png)')
 
-        background = QImage('./imgs/ui/background_star.jpeg')
-        # background.scaled(QSize(500,500))
-        palette = QPalette()
-        palette.setBrush(10,QBrush(background))
-        self.setPalette(palette)
+        # background = QImage('./imgs/ui/background_star.jpeg')
+        #
+        # background.scaled(QSize(64,64))
+        # palette = QPalette()
+        #
+        # palette.setBrush(10,QBrush(background))
+        # self.setAutoFillBackground(True)
+        # self.setPalette(palette)
 
         # set main btn
         self.vision_btn = QtWidgets.QPushButton('비전', self)
@@ -73,6 +99,7 @@ class AppForm(QtWidgets.QWidget):
 
 
         title_text = QtWidgets.QLabel('Vision Project with PyQt5')
+        title_text.setStyleSheet('color : white')
         title_text.setFont(QFont('Arial',35))
         title_text.setAlignment(Qt.AlignCenter)
 
@@ -107,13 +134,26 @@ class AppForm(QtWidgets.QWidget):
         reply = QtWidgets.QMessageBox.question(self, 'Message', '준비중입니다 : (',
                                        QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No, QtWidgets.QMessageBox.No)
 
+    def paintEvent(self, a0: QPaintEvent):
+        painter = QPainter(self)
+        painter.drawRect(self.rect())
+        pix = QPixmap("./imgs/ui/background-img.png")  # Change to the relative path of your own image
+
+        painter.drawPixmap(self.rect(), pix)
+
+
+
 # python main code(실제 실행되는 스크립트)
 if __name__ == '__main__':
     # QApplication 함수 호출을 통해 app 생성 (모든 QT Application은 어플리케이션 객체를 생성해야한다. doc 참조.
     app = QtWidgets.QApplication(sys.argv)
     # AppForm 인스턴스 생성 및 창 실행
     form = AppForm()
+    form.paintEngine()
+
     form.show()
+    form.playBackgroundSound()
+
 
     # App 호출, pyqt4와의 호환성을 위해 sys.exit(app.exec_())로 쓰기도
     app.exec_()
