ce58d10bb49b508183349495a1ed1097c0848570
ngost <hm5207@gmail.com>
2020-12-08 02:34:16 +0900

    pyqt5 Qmultimedia is suck. Never use. Alternatively, use multiprocessing, playsound module

--- Root/QtStart.py
+++ Root/QtStart.py
@@ -1,11 +1,11 @@
 # 모듈 임포트 sys, PyQt5의 QApplication과 QWidget 모듈, wxPython
 import sys, os
 import wx
-from PyQt5 import QtWidgets, QtMultimedia
-# from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QAction, qApp, QMainWindow
+import PyQt5
+from PyQt5 import QtWidgets
 from PyQt5.QtGui import *
-from PyQt5.QtCore import Qt,QSize,QDir,QUrl,QCoreApplication
-from PyQt5.QtMultimedia import QAudio, QAudioDeviceInfo, QAudioFormat, QAudioOutput,QMediaContent
+from PyQt5.QtCore import Qt,QUrl
+from PyQt5.QtMultimedia import QMediaContent
 # App 관련 Class 생성
 class AppForm(QtWidgets.QWidget):
     # 기본 스크린 사이즈
@@ -33,29 +33,7 @@ class AppForm(QtWidgets.QWidget):
     #
     #     # self.setGeometry(300, 300, 300, 200)
 
-    def playBackgroundSound(self):
 
-        CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-        filename = os.path.join(CURRENT_DIR, "bgm_sample.wav")
-        QtMultimedia.QSound.play(filename)
-
-        # filename = os.path.join(CURRENT_DIR, "bgm.mp3")
-        # player = QtMultimedia.QMediaPlayer()
-        # print(filename)
-        #
-        # def handle_state_changed(state):
-        #
-        #     if state == QtMultimedia.QMediaPlayer.PlayingState:
-        #         print("started")
-        #     elif state == QtMultimedia.QMediaPlayer.StoppedState:
-        #         print("finished")
-        #         QCoreApplication.quit()
-        #
-        # player.stateChanged.connect(handle_state_changed)
-        # url = QUrl.fromLocalFile(filename)
-        # player.setMedia(QtMultimedia.QMediaContent(url))
-        #
-        # player.play()
 
     def init_ui(self):
         #set title
@@ -141,19 +119,61 @@ class AppForm(QtWidgets.QWidget):
 
         painter.drawPixmap(self.rect(), pix)
 
+# def testFunction():
+#     print("test!")
+#     CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+#     filename = os.path.join(CURRENT_DIR, "bgm.mp3")
+#
+#     player = PyQt5.QtMultimedia.QMediaPlayer()
+#     playlist = PyQt5.QtMultimedia.QMediaPlaylist(player)
+#
+#     urls = QUrl.fromLocalFile(filename)
+#     playlist.addMedia(QMediaContent(urls))
+#     playlist.setPlaybackMode(PyQt5.QtMultimedia.QMediaPlaylist.Loop)
+#
+#     player.setPlaylist(playlist)
+#     player.play()
+#     print("play!")
+
+def playBackgroundSound():
+    from playsound import playsound
+    import multiprocessing
 
 
-# python main code(실제 실행되는 스크립트)
+    p = multiprocessing.Process(target= playsound, args= ['Root/bgm.mp3'])
+    # playsound('Root/bgm.mp3',False)
+    p.start()
+    # p.terminate()
+
+# python main code(실행 메인 스크립트)
 if __name__ == '__main__':
+
     # QApplication 함수 호출을 통해 app 생성 (모든 QT Application은 어플리케이션 객체를 생성해야한다. doc 참조.
     app = QtWidgets.QApplication(sys.argv)
+    # testFunction()
+    #도대체 왜이러나!!
+    # CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+    # filename = os.path.join(CURRENT_DIR, "bgm.mp3")
+    #
+    # player = QtMultimedia.QMediaPlayer()
+    # playlist = QtMultimedia.QMediaPlaylist(player)
+    #
+    # urls = QUrl.fromLocalFile(filename)
+    # playlist.addMedia(QMediaContent(urls))
+    # playlist.setPlaybackMode(QtMultimedia.QMediaPlaylist.Loop)
+    #
+    # player.setPlaylist(playlist)
+    # player.play()
+
+    # playBackgroundSound()
+
     # AppForm 인스턴스 생성 및 창 실행
+
     form = AppForm()
     form.paintEngine()
-
     form.show()
-    form.playBackgroundSound()
-
+    playBackgroundSound()
 
     # App 호출, pyqt4와의 호환성을 위해 sys.exit(app.exec_())로 쓰기도
-    app.exec_()
+
+    sys.exit(app.exec_())
